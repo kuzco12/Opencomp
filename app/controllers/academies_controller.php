d4935084bfa862eac9f94487799aba4d28412339
jtraulle <jtraulle@gmail.com>
2011-06-13 15:29:43 +0200

    Corrections dans le contrôleur des académies.
    Développement.

--- app/controllers/academies_controller.php
+++ app/controllers/academies_controller.php
@@ -1,58 +1,91 @@
 <?php
 
+/**
+ * Contrôleur de gestion des académies
+ *
+ * @category	Controller
+ * @package 	Opencomp
+ * @version 	1.0
+ * @author	Jean Traullé <jtraulle@gmail.com>
+ * @author	Gauthier Dulot <mrimgauthier@gmail.com>
+ * @license  	http://www.opensource.org/licenses/agpl-v3 The Affero GNU General Public License
+ * @link     	http://www.opencomp.fr
+ */
+
 class AcademiesController extends AppController
 {
+    
+    /**
+     * Méthode listant l'ensemble des académies existantes.
+     *
+     * @return void
+     * @access public
+     */
     function index()
     {
         $this->set('title_for_layout', __('Liste des Academies',true));
         $this->set('academies', $this->paginate());
     }
-    //----------------------------------------------
-    //Ajout d'une Academie    
-    function add($id = null)
+
+    /**
+     * Méthode permettant d'ajouter une académie.
+     *
+     * @return void
+     * @access public
+     */
+    function add()
     {
         $this->set('title_for_layout', __('Ajouter une Academie',true));
         
     }
     
-    //Edition d'une academie
-      function edit($id = null)
+    /**
+     * Méthode permettant d'éditer une académie.
+     *
+     * @return void
+     * @param int $id
+     * @access public
+     */
+    function edit($id = null)
     {
-        $this->set('title_for_layout', __('Modifier une Academie',true));
-    
-        
+        $this->set('title_for_layout', __('Modifier une Academie',true));   
     }
     
-    
-    //Suppression d'une Academie'
+    /**
+     * Méthode permettant de supprimer une académie.
+     *
+     * @return void
+     * @param int $id
+     * @access public
+     */
     function delete ($id)
     {
-    
         
     }
+    
+    /**
+     * Méthode permettant de visualiser les détails d'une académie.
+     *
+     * @return void
+     * @param int $id
+     * @access public
+     */
     function view($id)
     {
-	//On transmet le titre à la vue.
 	$this->set('title_for_layout', __('Affichage d\'une académie', true));
-
+	
 	if (!$id)
 	{
 	    $this->redirect(array('action' => 'index'));
 	}
 
-	//On essayes de récupérer dans $infoPupil les informations de la
-	//classe pour laquelle l'id a été passé en paramètre.
-	//Si l'élève n'existe pas (mauvais id), alors, la variable sera vide.
 	$infoAcademy = $this->Academy->read(null, $id);
 
-	//Avant d'afficher la page, on teste si $infoPupil est vide, si c'est
-	//le cas, on cours-circuite l'affichage en redirigeant l'utilisateur vers
-	//la liste des classe en affichant un message d'erreur.
 	if (!empty($infoAcademy))
 		$this->set('academy', $infoAcademy);
 	else
 	{
-		$this->Session->setFlash(__('L\'élève que vous souhaitez afficher n\'existe pas.', true), 'message_erreur');
+		$this->Session->setFlash(__('L\'académie que vous souhaitez afficher n\'existe pas.', true), 'message_erreur');
 		$this->redirect(array('action' => 'index'));
 	}
     }
