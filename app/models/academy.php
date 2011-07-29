328603172628bcffb0e24844a0e3826846bf1869
jtraulle <jtraulle@gmail.com>
2011-07-30 01:07:53 +0200

    Correction du modèle academy.php qui empêchait la sauvegarde des modifications d'une académie en raison de mauvaises règles de validation. fix #6
    Débogage.

--- app/models/academy.php
+++ app/models/academy.php
@@ -1,45 +1,22 @@
 <?php
 
-   class Academy extends AppModel
-	{
-               
-                var $validate = array(
-                    //Nom de l'academie
-                    'name' => array(
-                        'longueur' => array(
-				'rule' => array('minLength', 3),
-				'message' => 'Taille minimum de 3 caractères'
-                            ),
-                        
-                        
-                        'unique_create' => array(
-                            'rule' => 'isUnique',
-                            'on' => 'create',
-                            'message' => 'Cette academie existe déja.Veuillez saisir un autre nom!'
-                        ),
-                        'unique_update' => array(
-                            'rule' => 'isUniqueUpdate',
-                            'on' => 'update',
-                            'message' => 'Le nom de cette academie n\'est pas disponible,Veuillez en choisir un autre !'
-                          )
-                        ),
-                    );
-                     
-           //-------------------------------------------------------------         
-//                 var $validate=array(
-//                    'username'=>array(
-//                        'longueur'=>array(
-//                            'rule'=>array('minLenght',5),
-//                                'message'=>'Taille minimum de 5 caractères'
-//                        )
-//                    )
-//                    
-//                 );
+class Academy extends AppModel
+{
 
-        }
-        
-	 
-
-
+    var $validate = array(
+	
+	//Nom de l'academie
+	'name' => array(
+	    'longueur' => array(
+		'rule' => array('minLength', 3),
+		'message' => 'Taille minimum de 3 caractères'
+		),
+	    'unique_create' => array(
+		'rule' => 'isUnique',
+		'message' => 'Cette académie existe déjà, veuillez saisir un autre nom.'
+		)
+	    )
+	);
+}
 
 ?>
